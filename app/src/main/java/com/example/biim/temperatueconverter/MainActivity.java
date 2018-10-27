f8e2491d38795ad1bc2350ab0ac246061fc8f0eb
Chanakarn Paveenvittayaroj <be4mz@hotmail.com>
2018-10-27 11:26:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/biim/temperatueconverter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.biim.temperatueconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.biim.temperatueconverter.model.Body;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button fbutton = findViewById(R.id.f_button);
+        fbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.input_text);
+                int number = Integer.parseInt(input.getText().toString());
+                Body body = new Body(number);
+                final TextView output = findViewById(R.id.output_text);
+                String show = Integer.toString(body.tranform_c_to_f());
+                output.setText(show);
+            }
+        });
+        Button cbutton = findViewById(R.id.c_button);
+        cbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.input_text);
+                int number = Integer.parseInt(input.getText().toString());
+                Body body = new Body(number);
+                final TextView output = findViewById(R.id.output_text);
+                String show = String.format("%.2f", body.tranform_f_to_c());
+                output.setText(show);
+            }
+        });
+    }
+}
